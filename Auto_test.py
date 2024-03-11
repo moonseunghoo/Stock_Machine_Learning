71bebbd6112603da64beff818e911a44921699b0
moonseunghoo <tmdgn2002@gmail.com>
2024-03-11 13:19:36 +0900

    test

--- Auto_test.py
+++ Auto_test.py
@@ -152,7 +152,7 @@ def Prediction():
   # 불필요한 데이터 삭제
   filter_pred = filter_pred.drop({'Ticker','Date','Change'},axis=1) #종목코드, 날자, 상승율 삭제
 
-  model = �tf.keras.models.load_model("RaspberryPi_test.h5")
+  model = tf.keras.models.load_model("RaspberryPi_test.h5")
   
   # GRU_128_64_32_2_KOSPI_TI_3%.h5
   Pred = model.predict(filter_pred).round(2)
