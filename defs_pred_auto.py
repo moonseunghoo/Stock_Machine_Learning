342008ccda196267d705b2bbc43e7339208c3141
moonseunghoo <tmdgn2002@gmail.com>
2024-03-11 18:09:22 +0900

    test

--- defs_pred_auto.py
+++ defs_pred_auto.py
@@ -313,7 +313,9 @@ def scrap_stock_data(code,marcap, end_info_day, day_120):
 def scrap_sub_data(end_info_day, day_120, day_21):
     warnings.simplefilter(action='ignore', category=FutureWarning) # FutureWarning 제거
 
-    KSI_df = fdr.DataReader('KS11', day_21, end_info_day).reset_index().drop(['Adj Close'], axis=1).round(2)
+    KSI_df = fdr.DataReader('KS11', day_21, end_info_day).reset_index().round(2)
+    # .drop(['Adj Close'], axis=1)
+    print(KSI_df.columns)
     print(KSI_df.columns)
     #asd
 
